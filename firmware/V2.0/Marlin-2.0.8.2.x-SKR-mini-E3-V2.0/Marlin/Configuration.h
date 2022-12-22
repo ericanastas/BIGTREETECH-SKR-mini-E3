eebc8443afd104f11afed0afba5786b22b4ffef1
Eric Anastas <aireq303@gmail.com>
2022-12-21 21:15:05 -0800

    Set Y_MIN_POS after moving Y limit switch

--- firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -1243,7 +1243,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -1
-#define Y_MIN_POS -16
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 25 // Allow head to move off right side of bed for ABL probing
 #define Y_MAX_POS Y_BED_SIZE
